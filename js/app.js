090a162c7c759bb32ecd461f2dd979c6fe379e9d
dmitry <akulinind@gmail.com>
2020-12-27 12:48:58 +0300

    add timeout to win condition

--- js/app.js
+++ js/app.js
@@ -2,47 +2,39 @@ const playerStartX = 200;
 const playerStartY = 400;
 const stepX = 100;
 const stepY = 85;
-
+const finishLineY = 0;
+const rightBorder = 500;
 const enemyStartX = 0;
 const enemyMinSpeed = 100;
-const enemyMaxSpeed = 300;
-const enemyStartYArray = [60, 145, 230]
+const enemyMaxSpeed = 400;
+const enemyStartYPositions = [60, 145, 230]
 
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-// Enemies our player must avoid
+function makeRound100(val) {
+    return Math.round(val / 100) * 100;
+}
+
 var Enemy = function (y) {
     this.x = enemyStartX;
     this.y = y;
     this.speed = getRandomInteger(enemyMinSpeed, enemyMaxSpeed);
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function (dt) {
     this.x += this.speed * dt;
-    if (this.x > 600) {
+    if (this.x > rightBorder) {
         this.x = enemyStartX;
     }
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 let Player = function (x,y) {
     this.x = x;
@@ -51,21 +43,25 @@ let Player = function (x,y) {
 }
 
 Player.prototype.update = function () {
-    console.log('upd')
     allEnemies.forEach(enemy => {
-        // if (this.x === enemy.x) {
-        if ((makeRound10(this.x) === makeRound10(enemy.x)) && (this.y === enemy.y)) {
-            alert(enemy.x);
+        if ((makeRound100(this.x) === makeRound100(enemy.x)) && (this.y === enemy.y)) {
+            alert('You lose');
             this.x = playerStartX;
             this.y = playerStartY;
         };
     })
+    if (this.y < finishLineY) {
+        setTimeout(() => {
+            this.y = playerStartY;
+            alert('You win!');
+        }, 10);
+    }
 };
 
 Player.prototype.handleInput = function (key) {
     if (key === 'left' && this.x - stepX >= 0) {
         this.x -= stepX;
-    } else if (key === 'right' && this.x + stepX <= 500 - stepX) {
+    } else if (key === 'right' && this.x + stepX <= rightBorder - stepX) {
         this.x += stepX;
     } else if (key === 'down' && this.y + stepY <= playerStartY) {
         this.y += stepY;
@@ -78,28 +74,14 @@ Player.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-let enemy1 = new Enemy(230, 900);
-let enemy2 = new Enemy(145, 900);
-let enemy3 = new Enemy(60, 900);
-const allEnemies = [enemy1, enemy2,enemy3];
+const allEnemies = [];
+for (let i = 0; i < 3; i++) {
+    allEnemies.push(new Enemy(enemyStartYPositions[i]));
+}
+
 let player = new Player(playerStartX, playerStartY);
 
 
-function getRandomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function makeRound10(val) {
-    return Math.round(val / 100) * 100;
-}
-
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
